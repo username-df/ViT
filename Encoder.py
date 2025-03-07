653a27a6c24aa2087109fd3b7f0ba3a708e0964c
username-df <usernamedf@outlook.com>
2025-03-06 19:15:34 -0500

    TransformerEncoder class

---
+++ Encoder.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn
+from Attention import multiHeadAttention
+
+class Encoder(nn.Module):
+    def __init__(self, embed_dim):
+        super().__init__()
+        
+        self.MSA = multiHeadAttention(embed_dim, num_heads=4)
+
+        self.MLP = nn.Sequential([
+            nn.Linear(embed_dim, embed_dim),
+            nn.GELU(),
+            nn.Linear(embed_dim, embed_dim)
+        ])
+
+        self.LN1 = nn.LayerNorm(embed_dim)
+        self.LN2 = nn.LayerNorm(embed_dim)
+
+    def forward(self, x):
+        x = x + self.MSA(self.LN1(x))
+        x = x + self.MLP(self.LN2(x))
+        return x
