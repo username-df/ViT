47ca0c3e947a4b3464583fca236135405c3dcab6
username-df <usernamedf@outlook.com>
2025-03-09 01:28:39 -0500

    added num_patches argument in constructor

--- PatchEmbed.py
+++ PatchEmbed.py
@@ -3,10 +3,8 @@ from torch import nn
 from einops.layers.torch import Rearrange
 
 class PatchEmbed(nn.Module):
-    def __init__(self, img_size, patch_size, embed_dim):
+    def __init__(self, patch_size, num_patches, embed_dim):
         super().__init__()
-        num_patches = (img_size ** 2) // (patch_size ** 2)
-
         self.get_patches = Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=patch_size, p2=patch_size)
         self.lin_proj = nn.Linear(3*patch_size**2, embed_dim) # linear projection of patches
         self.pos_enc = nn.Parameter(torch.randn(num_patches+1, embed_dim), requires_grad=True) # learnable positional encoding
