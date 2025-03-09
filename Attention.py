55e619b3dc0e8603a1ef8afcdb9743d45d5e9759
username-df <usernamedf@outlook.com>
2025-03-09 00:50:14 -0500

    assert embed_dim % num_heads == 0

--- Attention.py
+++ Attention.py
@@ -26,6 +26,7 @@ class SelfAttention(nn.Module):
 class MultiHeadAttention(nn.Module):
     def __init__(self, embed_dim, num_heads):
         super().__init__()
+        assert (embed_dim % num_heads == 0), "Embed dimension can not be divided by number of heads."
         self.head_size = embed_dim // num_heads
         self.HeadsList = nn.ModuleList([SelfAttention(embed_dim, self.head_size) for _ in range(num_heads)])
         self.output_proj = nn.Linear(embed_dim, embed_dim)
