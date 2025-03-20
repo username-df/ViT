efdff6cd13814c45f11b568135cd79d2b5d72a77
username-df <usernamedf@outlook.com>
2025-03-20 18:35:14 -0400

    stochastic depth instead of dropout

--- Encoder.py
+++ Encoder.py
@@ -1,9 +1,9 @@
-import torch
+import torchvision.ops as ops
 from torch import nn
 from Attention import MultiHeadAttention
 
 class Encoder(nn.Module):
-    def __init__(self, embed_dim, num_heads):
+    def __init__(self, embed_dim, num_heads, p):
         super().__init__()
         
         self.MSA = MultiHeadAttention(embed_dim, num_heads)
@@ -17,9 +17,9 @@ class Encoder(nn.Module):
         self.LN1 = nn.LayerNorm(embed_dim)
         self.LN2 = nn.LayerNorm(embed_dim)
 
-        self.dropout = nn.Dropout(p=0.1)
+        self.drop_path = ops.StochasticDepth(p=p, mode="batch")
 
     def forward(self, x):
-        x = x + self.MSA(self.LN1(x))
-        x = x + self.dropout(self.MLP(self.LN2(x)))
+        x = x + self.drop_path(self.MSA(self.LN1(x)))
+        x = x + self.drop_path(self.MLP(self.LN2(x)))
         return x
