9df7e4f101bab03ea8db5dbbea2634d0ec138373
username-df <usernamedf@outlook.com>
2025-03-23 12:20:45 -0400

    added color jitter to transformations

--- dataset.py
+++ dataset.py
@@ -4,14 +4,15 @@ from torch.utils.data import DataLoader
 
 transform = transforms.Compose([
     transforms.RandomHorizontalFlip(),
+    transforms.ColorJitter(0.1, 0.1, 0.1),
     transforms.RandomResizedCrop((32, 32)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],
                          std=[0.2023, 0.1994, 0.2010])
 ])
 
-train_dataset = datasets.CIFAR100(root='./cifar100', train=True, download=False, transform=transform)
-test_dataset = datasets.CIFAR100(root='./cifar100', train=False, download=False, transform=transform)
+train_dataset = datasets.CIFAR10(root='./cifar10', train=True, download=False, transform=transform)
+test_dataset = datasets.CIFAR10(root='./cifar10', train=False, download=False, transform=transform)
 
 train_data = DataLoader(train_dataset, batch_size=512, shuffle=True)
 test_data = DataLoader(test_dataset, batch_size=512, shuffle=False)
